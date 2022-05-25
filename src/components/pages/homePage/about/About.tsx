3d7c202858bbb93e785536b6d5e4e0236dd1f280
Lian Nidam <58288314+Razelkabets@users.noreply.github.com>
2022-05-25 17:36:41 +0300

    Edit About Us content

--- src/components/pages/homePage/about/About.tsx
+++ src/components/pages/homePage/about/About.tsx
@@ -5,14 +5,15 @@ import GreyCard from "../../../layout/card/GreyCard";
 const About = () => {
   return (
     <GreyCard>
-        <div className="about-container">
+      <div className="about-container">
         <div className="about-description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a lacus
-          vel justo fermentum bibendum non eu ipsum. Cras porta malesuada eros,
-          eget blandit turpis suscipit at. Vestibulum sed massa in magna sodales
-          porta. Vivamus elit urna, dignissim a vestibulum. <br /> <br />
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a lacus
-          vel justo fermentum bibendum non eu ipsum. Cras porta malesuada eros.
+          A social website designed to mediate between the need of associations
+          for volunteers, and volunteers who want to come and volunteer. The
+          site unites associations in all kinds of places in the country and
+          allows individual people or huge companies to reach the associations
+          they have chosen and volunteer by filtering by location or type of
+          volunteering. The client is allowed to choose to volunteer at the 90th
+          minute or to volunteer on a date that suits him.
         </div>
         <img src={aboutLogo} alt="about" className="about-logo"></img>
       </div>
