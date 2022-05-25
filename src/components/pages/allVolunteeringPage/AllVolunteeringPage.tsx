1c433a88f28277241c4f1a13b4367a93b17a4935
Lian Nidam <58288314+Razelkabets@users.noreply.github.com>
2022-05-25 11:00:13 +0300

    Filter dates- Volunteering page

--- src/components/pages/allVolunteeringPage/AllVolunteeringPage.tsx
+++ src/components/pages/allVolunteeringPage/AllVolunteeringPage.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import Footer from '../../footer/Footer'
 import Header from '../../header/Header'
 import VolunteeringGrid from './VolunteeringGrid'
 
 const AllVolunteeringPage = () => {
+  const [filter, setFilter] = useState("all");
+
   return (
     <div className="page-container">
       <Header />
       <h2>Volunteering</h2>  
-       <VolunteeringGrid /> 
+      <div className="categories-div">
+        <button onClick={() => setFilter("all")} className={`${filter === "all" ? 'all':'filterLink'}`}>All</button>
+        <button onClick={() => setFilter("today")} className={`${filter === "today" ? 'today':'filterLink'}`}>Today</button>        
+        <button onClick={() => setFilter("tommorrow")} className={`${filter === "tommorrow" ? 'tommorrow':'filterLink'}`}>Tommorrow</button>
+      </div>
+      <br /> 
+       <VolunteeringGrid filter={filter} /> 
     </div>  )
 }
 
--- src/components/pages/allVolunteeringPage/VolunteeringGrid.tsx
+++ src/components/pages/allVolunteeringPage/VolunteeringGrid.tsx
@@ -1,59 +1,93 @@
-import { Fragment, useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
-import { fetchVolunteering } from '../../../store/volunteeringDataSlice';
-import { fetchAssociations } from '../../../store/AssociationDataSlice';
-import { useAppDispatch } from '../../../store/store';
-import "./AllVolunteeringPage.scss"
-import SubmitButton from '../../layout/button/SubmitButton';
-import VolunteeringPage from '../volunteeringPage/VolunteeringPage';
-const VolunteeringGrid = () => {
+import { Fragment, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { fetchVolunteering } from "../../../store/volunteeringDataSlice";
+import { fetchAssociations } from "../../../store/AssociationDataSlice";
+import { useAppDispatch } from "../../../store/store";
+import "./AllVolunteeringPage.scss";
+import SubmitButton from "../../layout/button/SubmitButton";
+import VolunteeringPage from "../volunteeringPage/VolunteeringPage";
 
-    const dispatch = useAppDispatch();
-    const { volunteering } = useSelector((state: any) => state.volunteering);
-  
-    useEffect(() => {
-      dispatch(fetchVolunteering());
-      dispatch(fetchAssociations());
-    }, []);
+const VolunteeringGrid = ({ filter }: any) => {
+  const dispatch = useAppDispatch();
+  const { volunteering } = useSelector((state: any) => state.volunteering);
 
-    const [openVolunteeringCard, setOpenVolunteeringCard] = useState(false);
-    const [volunteeringName, setVolunteeringName] = useState("");
-    
-    const openVolunteeringCardHandler = (volunteer: string) => {
-      setOpenVolunteeringCard(true);
-      setVolunteeringName(volunteer);
-    };
+  useEffect(() => {
+    dispatch(fetchVolunteering());
+    dispatch(fetchAssociations());
+  }, []);
 
-    return (
-      <Fragment>
-         {openVolunteeringCard && (
-           <Fragment>
-             <button
-             className="volunteering-container"
-             onClick={() => setOpenVolunteeringCard(false)}
-           ></button>
-           <button
-             className="close-volunteering-page"
-             onClick={() => setOpenVolunteeringCard(false)}
-           >
-             X
-           </button>
-           <br />
+  const [openVolunteeringCard, setOpenVolunteeringCard] = useState(false);
+  const [volunteeringName, setVolunteeringName] = useState("");
+
+  const openVolunteeringCardHandler = (volunteer: string) => {
+    setOpenVolunteeringCard(true);
+    setVolunteeringName(volunteer);
+  };
+
+  const passFilter = (item: any) => {
+    const volunteeringDay = item.date.slice(0, 9);
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const nextDay = date.getDate() + 1;
+    const today = date.getFullYear() + "-" + month + "-" + date.getDate();
+    const tommorrow = date.getFullYear() + "-" + month + "-" + nextDay;
+    switch (filter) {
+      case "all":
+        return true;
+      case "today":
+        if (today === volunteeringDay) return true;
+        else return false;
+      case "tommorrow":
+        if (tommorrow === volunteeringDay) return true;
+        else return false;
+      default:
+        return true;
+    }
+  };
+
+  return (
+    <Fragment>
+      {openVolunteeringCard && (
+        <Fragment>
+          <button
+            className="volunteering-container"
+            onClick={() => setOpenVolunteeringCard(false)}
+          ></button>
+          <button
+            className="close-volunteering-page"
+            onClick={() => setOpenVolunteeringCard(false)}
+          >
+            X
+          </button>
+          <br />
           <VolunteeringPage volunteering={volunteeringName}></VolunteeringPage>
-          </Fragment>
+        </Fragment>
       )}
-        <div className='feed'>
-        {!openVolunteeringCard && volunteering.map((item: any) => (
-              <div className='feed-container' key={item.name}>
-                  <div><h1>{item.name}</h1></div>
-                  <div><h3>{item.date}</h3></div>
-                  <div><h3>{item.address}</h3></div>
-                  <SubmitButton value={"Sign up here"} onClick={() => openVolunteeringCardHandler(item.name)}></SubmitButton>
-              </div>
-          ))}
-        </div>
-      </Fragment>
-    );
-}
+      <div className="feed">
+        {!openVolunteeringCard &&
+          volunteering.map((item: any) => {
+            if (passFilter(item))
+              return (
+                <div className="feed-container" key={item.name}>
+                  <div>
+                    <h1>{item.name}</h1>
+                  </div>
+                  <div>
+                    <h3>{item.date}</h3>
+                  </div>
+                  <div>
+                    <h3>{item.address}</h3>
+                  </div>
+                  <SubmitButton
+                    value={"Sign up here"}
+                    onClick={() => openVolunteeringCardHandler(item.name)}
+                  ></SubmitButton>
+                </div>
+              );
+          })}
+      </div>
+    </Fragment>
+  );
+};
 
 export default VolunteeringGrid;
