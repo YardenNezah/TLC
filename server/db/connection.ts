a7bfd23db2df877588335eb70af76f855d9401e1
Lian Nidam <58288314+Razelkabets@users.noreply.github.com>
2022-06-13 09:52:38 +0300

    Connection between founders to db

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,11 +1,43 @@
 import mongoose from "mongoose";
 import founder from "./models/founders.js";
-import volunteering from "./models/volunteering.js";
+
+const dbInit = async () => {
+  const founderCount = await founder.countDocuments();
+  if (founderCount === 0) {
+    founder.insertMany([
+      {
+        founderName: "Yarden Tsaraf",
+        founderDescription: "student",
+        founderImage: "https://i.ibb.co/9rGVQDT/yarden.jpg",
+        founderLinkedin: "https://www.linkedin.com/in/yarden-tsaraf",
+        founderEmail: "yardentsaraf@gmail.com",
+        founderAge: 24,
+      },
+      {
+        founderName: "Raz Elkabets",
+        founderDescription: "student",
+        founderImage: "https://i.ibb.co/QdbysSB/raz.jpg",
+        founderLinkedin: "https://www.linkedin.com/in/yarden-tsaraf",
+        founderEmail: "razelkabets050@gmail.com",
+        founderAge: 25,
+      },
+      {
+        founderName: "Lian Nidam",
+        founderDescription: "student",
+        founderImage: "https://i.ibb.co/TBjCQnG/lian.jpg",
+        founderLinkedin: "https://www.linkedin.com/in/yarden-tsaraf",
+        founderEmail: "liannidam04@gmail.com",
+        founderAge: 24,
+      },
+    ]);
+  }
+};
 
 const connection = mongoose.connect(
   "mongodb+srv://tlc:tlc1234@cluster0.lrytr.mongodb.net/TLC?retryWrites=true&w=majority",
   () => {
     console.log("db connected successfuly.");
+    dbInit();
   }
 );
 
